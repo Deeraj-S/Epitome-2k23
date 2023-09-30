1b2beb41264cb66a632b96001facd31d235864f3
Deeraj22 <sdeeraj2000@gmail.com>
2023-09-30 14:13:01 +0530

    Created folder component - to add all the component files

---
+++ front-end/src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Navbar() {
+    return (
+        <>
+            <div>
+                Navigation bar
+            </div>
+        </>
+    )
+}
+
+export default Navbar
