924b623e3197d92db0cf402f34512795e79a7e8d
Deeraj22 <sdeeraj2000@gmail.com>
2023-09-30 14:14:31 +0530

    Created file structure for Navigation bar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navbar />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
