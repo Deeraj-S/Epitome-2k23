60da5a7cedceb44b0ae62d64ddf8745fd7ba8df3
Deeraj22 <sdeeraj2000@gmail.com>
2023-10-09 18:53:27 +0530

    Added event poster divs

--- front-end/src/Components/Home.js
+++ front-end/src/Components/Home.js
@@ -2,6 +2,8 @@ import React from "react"
 import Navbar from "./Navbar"
 import { useNavigate } from "react-router-dom"
 import '../styles/Home.css'
+import image from '../img2.png'
+
 
 function Home() {
     const navigate = useNavigate();
@@ -9,8 +11,25 @@ function Home() {
     return (
         <>
             <Navbar />
-            <div className="bg"></div>
-
+            <div className="bg-image"></div>
+            <div className="logo-image"><div className="logo-holder"><img src={image} alt="logo" className="img-logo" /></div></div>
+            <div>
+                <h1>EVENT POSTERS</h1>
+                <div className="event-box">
+                    <div className="box1"></div>
+                    <div className="box1"></div>
+                    <div className="box1"></div>
+                    <div className="box1"></div>
+                    <div className="box1"></div>
+                    <div className="box1"></div>
+                    <div className="box1"></div>
+                    <div className="box1"></div>
+                    <div className="box1"></div>
+                    <div className="box1"></div>
+                    <div className="box1"></div>
+                    <div className="box1"></div>
+                </div>
+            </div>
         </>
     )
 }
