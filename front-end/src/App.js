94cdbce9c64f294de8e5a93db52d12d9c5c44825
Manasi <manasig2002@gmail.com>
2023-10-13 11:55:37 +0530

    added forgot password and change password

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Home from './Components/Home';
 import SignUp from './Components/Signup';
 import SignIn from './Components/Signin';
+import ForgotPass from './Components/ForgotPass';
+import ChangePass from './Components/ChangePass';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route path='/' element={<Home />}></Route>
           <Route path='/signup' element={<SignUp />}></Route>
           <Route path='/signin' element={<SignIn />}></Route>
+          <Route path='/forgotpass' element={<ForgotPass />}></Route>
+          <Route path='/newpass' element={<ChangePass />}></Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ front-end/src/Components/ChangePass.js
@@ -0,0 +1,66 @@
+import { useState } from "react"
+
+import { useNavigate } from "react-router-dom"
+
+function ChangePass() {
+
+    const navigate = useNavigate()
+    const [otp, setOtp] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = () => {
+        console.log(otp, password)
+        /*axios.post('http://localhost:5000/submit-otp',
+            {
+                otp: otp,
+                password: password,
+            })
+            .then(res => {
+                console.log(res.data)
+                if (res.data.code === 200) {
+                    navigate('/signin')
+                    alert('Password Updated.')
+                } else {
+                    alert('server err / wrong OTP')
+                }
+            }).catch(err => {
+                console.log(err)
+            })*/
+    }
+    const handleOtpChange = (event) => {
+        setOtp(event.target.value);
+      };
+    
+      const handlepassChange = (event) => {
+        setPassword(event.target.value);
+      };
+    
+
+    return (
+        <div className='login-body'>
+        <div className='login-box'>
+        <p>Forget password</p>
+    
+        <form onSubmit={handleSubmit}>
+        <div className='user-box'>
+        <input type="text" name="otp" value={otp} onChange={handleOtpChange} required/>
+        <label>OTP</label>
+        </div>
+        <div className='user-box'>
+        <input type="password" name="password" value={password} onChange={handlepassChange} required/>
+        <label>NEW PASSWORD</label>
+        </div>
+        <a href='./SignIn' onClick={handleSubmit}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Change Password
+        </a>
+        </form>
+        </div>
+        </div>
+      )
+}
+
+export default ChangePass
