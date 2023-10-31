f5c9ede0e7597731d586c643e2e58bad06ac6397
Manasi <manasig2002@gmail.com>
2023-10-31 21:21:13 +0530

    modified

--- front-end/src/Components/Media.js
+++ front-end/src/Components/Media.js
@@ -6,7 +6,7 @@ function Media() {
         <>
             <div className="centerdiv">
 
-                <div className="media-holder"><a href="https://instagram.com" target="blank"><i class="fa-brands fa-instagram"></i></a></div>
+                <div className="media-holder"><a href="https://instagram.com/epitome_2k23?igshid=NzZlODBkYWE4Ng==" target="blank"><i class="fa-brands fa-instagram"></i></a></div>
                 <div className="media-holder"><a href="https://youtube.com" target="blank"><i class="fa-brands fa-youtube" ></i></a></div>
                 <div className="media-holder"><a href="mailto:sdeeraj2000@gmail.com" target="_blank" rel="noreferrer"><i class="fa-solid fa-envelope"></i></a></div>
             </div>
